96173b71e049ab0e260840249edcb49f82a8ddea
Ayomide Oyewole <129783781+Ayomide0811@users.noreply.github.com>
2024-05-28 16:05:21 +0100

    Add files via upload
    BHU/22/04/05/0002
    Ayomide Oyewole David

---
+++ Encapsulation/physics.java
@@ -0,0 +1,10 @@
+public class physics {
+
+    float calVelocity (float distance, int time){
+        return (distance/time);
+    }
+
+    float calVelocity (float initialVelocity, float acceleration, int time){
+        return initialVelocity + (acceleration/time);
+    }
+}
