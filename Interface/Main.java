9c2e8024dbe17f3dc25b8ee34e025ac64cc91e8c
Ayomide Oyewole <129783781+Ayomide0811@users.noreply.github.com>
2024-06-03 03:13:00 +0100

    Delete Interface directory

--- Interface/Main.java
+++
@@ -1,8 +0,0 @@
-import org.w3c.dom.ls.LSOutput;
-
-public class Main {
-    public static void main(String[] args) {
-        Computer computer = new Computer(new RAM(), new processor(), new storage());
-        System.out.println(computer);
-    }
-}
