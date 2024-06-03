8006956ccf7792ca36d7b4b6ebc4e9fa737a1a1b
Ayomide Oyewole <129783781+Ayomide0811@users.noreply.github.com>
2024-06-03 03:22:29 +0100

    Add files via upload
    BHU/22/04/05/0002
    Ayomide Oyewole David

---
+++ StacksAndQueue/arrayDemo.java
@@ -0,0 +1,18 @@
+package StacksAndQueue;
+
+public class arrayDemo {
+    public static void main(String[] args) {
+        int[] numbersArray = new int[10];
+        numbersArray[0] = 17;
+        numbersArray[1] = 34;
+        numbersArray[2] = 56;
+        numbersArray[3] = 78;
+        numbersArray[4] = 90;
+
+        for (int i = 0; i <= numbersArray.length; i++) {
+            System.out.println(numbersArray[i]);
+
+        }
+
+    }
+}
