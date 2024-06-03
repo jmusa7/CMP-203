590b43eb66b768e75292b0ad7eace33ccb3ec328
Ayomide Oyewole <129783781+Ayomide0811@users.noreply.github.com>
2024-06-03 02:50:14 +0100

    Delete OOP_1 directory

--- OOP_1/Admin.java
+++
@@ -1,9 +0,0 @@
-package OOP_1;
-
-public class Admin extends User{
-    String staffNo;
-    String uploadResult(String course,Float score){
-        System.out.println(course=" "+ score);
-        return course;
-    }
-}
